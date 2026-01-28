f18bc6c3dca4760a8c9943ae014389b4853b140a
andrei-klimenko <andreib1plus@gmail.com>
2026-01-29 00:25:49 +0300

    Add

---
+++ create_raid.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Создаем raid
+mdadm --zero-superblock --force /dev/sd{b,c,d,e,f}
+mdadm --create --verbose /dev/md0 -l 6 -n 5 /dev/sd{b,c,d,e,f}
+#Вывод проверки
+mdadm -D /dev/md0 
+
+#Создаем GPT таблицу для 5 разделов
+parted -s /dev/md0 mklabel gpt
+parted /dev/md0 mkpart primary ext4 0% 20%
+parted /dev/md0 mkpart primary ext4 20% 40%
+parted /dev/md0 mkpart primary ext4 40% 60%
+parted /dev/md0 mkpart primary ext4 60% 80%
+parted /dev/md0 mkpart primary ext4 80% 100%
+
+#Создаем фс и мантируем по каталогам
+for i in $(seq 1 5); do sudo mkfs.ext4 /dev/md0p$i; done
+mkdir -p /raid/part{1,2,3,4,5}
+for i in $(seq 1 5); do mount /dev/md0p$i /raid/part$i; done
